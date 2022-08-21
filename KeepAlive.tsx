cae1eda1b7ad1e86732ac7d122e3cde4b6525813
Giovanni Piccolo <piccolog93@gmail.com>
2022-08-20 20:44:12 -0400

    add functionalities

--- KeepAlive.tsx
+++ KeepAlive.tsx
@@ -1,39 +1,63 @@
 import React, { useEffect } from 'react';
 
-console.log('Initiated keep-alive')
-
 interface KeepAliveProps {
-    url: string;
-    sleep?: number;
+  url: string;
+  sleep?: number;
+  delay?: number;
+  maxIdleTime?: number;
+  eventDetection?: boolean;
 }
 
 function KeepAlive(props: KeepAliveProps) {
 
-    const url = props.url;
-    const sleep = props.sleep == null ? 5 * 60 * 1000 : props.sleep;
+  const url: string = props.url;
+  const sleep: number = props.sleep == null ? 5 * 60 * 1000 : props.sleep;
+  const delay: number = props.delay == null ? 0 : props.delay;
+  const eventDetection: boolean = props.eventDetection == null ? false : props.eventDetection;
+  const maxIdleTime: number = props.maxIdleTime == null ? props.sleep! : props.maxIdleTime;
 
-    function handleErrors(response: { ok: any; statusText: string | undefined; }) {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        return response;
-    }
+  let lastEventDateTime: number = new Date().getTime();
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            fetch(url!)
-                .then(handleErrors)
-                .catch(() => {
-                        console.log('An error occurred calling keep-alive endpoint')
-                    }
-                )
-        }, sleep)
-        return () => clearInterval(interval);
+  ['keydown', 'mousemove', 'mousedown'].forEach(event => {
+    document.addEventListener(event, () => {
+      lastEventDateTime = new Date().getTime()
     })
+  })
 
-    return (
-        <div/>
-    )
+  function handleErrors(response: { ok: any; statusText: string | undefined; }) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
+
+  function isIdle(): boolean {
+    if (eventDetection) {
+      return lastEventDateTime + maxIdleTime <= new Date().getTime();
+    }
+    return true;
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const interval = setInterval(() => {
+        if (isIdle()) {
+          fetch(url!)
+            .then(handleErrors)
+            .catch((e) => {
+                console.warn('Keep-alive endpoint responded with an error: ', e.message)
+              }
+            )
+        }
+      }, sleep)
+      return () => clearInterval(interval);
+    }, delay);
+    return () => clearTimeout(timeout);
+  })
+
+  return (
+    <div/>
+  )
 }
 
 export default KeepAlive;
--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # KEEP-ALIVE
+
 Simple keep-alive React component to prevent session expiration and Google App Engine shutdown.
 
-## Description
-Just copy-pasta this component inside the project and activate it in the pages or components on which the user may not be triggering any HTTP call for a significant amount of time but you still need to keep the session, or back-end service, alive (e.g. big forms).
-
 ## Usage
-KeepAlive component accepts two parameters:
-- url [**mandatory**]
-- sleep [**optional**] (default: 5 minutes)
+
+Just _copy-pasta_ this component inside the project and activate it in the pages or components on which the user may not be triggering any HTTP call for a significant amount of time
+but you still need to keep the session, or back-end service, alive (e.g. big forms).
+
+KeepAlive component accepts five parameters:
+
+- url [string, **required**] - the URL to point to for the HTTP keep-alive calls.
+- sleep [number, optional, default: 5 minutes] - the time in ms between two subsequent keep-alive calls.
+- delay [number, optional, default: 0] - the time in ms before the component starts executing its function.
+- maxIdleTime [number, optional, default: _sleep_ parameter] - the time in ms between user actions after which it's considered idle. Only considered if _eventDetection_ is enabled.
+- eventDetection [number, optional, default: false] - enable or disable the listeners for _keydown_, _mousedown_ and _mousemove_ events.
 
 The **url** is mandatory and may point to an external resource or to a specific endpoint of your backend service.
-The **sleep** parameter is optional and represents the time in **milliseconds** between two consecutive keep alive pings. If not set, the default interval between pings is 5 minutes.
 
 ### Syntax
+
 ```html
-<KeepAlive url={'https://httpstat.us/200'} gap={1500}/>
+<KeepAlive
+        url={'https://httpstat.us/200'}
+        sleep={2000}
+        delay={3000}
+        maxIdleTime={5000}
+        eventDetection={true}
+/>
 ```
